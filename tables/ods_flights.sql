1e9c7bf3d21fc8d6aef773e5b5e37b904ea76e7e
moutesse <161531439+moutesse@users.noreply.github.com>
2024-12-22 04:09:57 +0300

    Create ods_flights.sql

---
+++ tables/ods_flights.sql
@@ -0,0 +1,21 @@
+--так как слой ods это сырые данные, все столбцы просто текстовые
+CREATE TABLE ods.flights (
+	"year" text NULL,
+	quarter text NULL,
+	"month" text NULL,
+	fl_date text NULL,
+	op_unique_carrier text NULL,
+	tail_num text NULL,
+	op_carrier_fl_num text NULL,
+	origin text NULL,
+	dest text NULL,
+	crs_dep_time text NULL,
+	dep_time text NULL,
+	dep_delay_new text NULL,
+	cancelled text NULL,
+	cancellation_code text NULL,
+	air_time text NULL,
+	distance text NULL,
+	weather_delay text NULL,
+	processed_dttm timestamp DEFAULT now() NULL
+);
