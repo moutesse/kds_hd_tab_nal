45028f59cb12e0e10a081c1d7a54278f2355239d
moutesse <161531439+moutesse@users.noreply.github.com>
2024-12-23 01:31:38 +0300

    Update etl_id_translate.sql

--- procedures/etl_id_translate.sql
+++ procedures/etl_id_translate.sql
@@ -3,13 +3,13 @@
 CREATE PROCEDURE etl.id_translate()
 LANGUAGE sql
 AS $$
-    TRUNCATE TABLE etl.id_translate_airport;
-    INSERT INTO etl.id_translate_airport (id, icao_code, iata_code)
+    TRUNCATE TABLE etl.id_translate_airport33;
+    INSERT INTO etl.id_translate_airport33 (id, icao_code, iata_code)
     SELECT 
         id,
         gps_code AS icao_code,
         iata_code
-    FROM dds.airport_code WHERE gps_code IS NOT null and iata_code IS NOT NULL;
+    FROM dds.airport_code33 WHERE gps_code IS NOT null and iata_code IS NOT NULL;
 $$;
 
 call etl.id_translate();
