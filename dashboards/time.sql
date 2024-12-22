efdcb41782e3fd70aad023fe7f48b4e6747ae7e9
moutesse <161531439+moutesse@users.noreply.github.com>
2024-12-23 01:25:50 +0300

    Update time.sql

--- dashboards/time.sql
+++ dashboards/time.sql
@@ -1,10 +1,10 @@
 -- https://datalens.yandex/0nnj43xa6sp0m
 
 SELECT EXTRACT(HOUR FROM flight_scheduled_ts) AS flight_hour, SUM(cancelled) AS cancelled
-FROM mart.fact_departure GROUP BY flight_hour ORDER BY flight_hour;
+FROM mart.fact_departure33 GROUP BY flight_hour ORDER BY flight_hour;
 
 SELECT EXTRACT(HOUR FROM flight_scheduled_ts) AS flight_hour, ABS(AVG(dep_delay_min)) AS earlier
-FROM mart.fact_departure WHERE dep_delay_min <= 0 GROUP BY flight_hour ORDER BY flight_hour;
+FROM mart.fact_departure33 WHERE dep_delay_min <= 0 GROUP BY flight_hour ORDER BY flight_hour;
 
 SELECT EXTRACT(HOUR FROM flight_scheduled_ts) AS flight_hour, AVG(dep_delay_min) AS delay
-FROM mart.fact_departure WHERE dep_delay_min > 0 GROUP BY flight_hour ORDER BY flight_hour;
+FROM mart.fact_departure33 WHERE dep_delay_min > 0 GROUP BY flight_hour ORDER BY flight_hour;
